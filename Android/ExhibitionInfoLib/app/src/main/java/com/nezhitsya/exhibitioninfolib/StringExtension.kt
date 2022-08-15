14a2158a928eb86def6d528445fb656a5ce48c12
nezhitsya <ldy9295@gmail.com>
2022-08-16 00:40:19 +0900

    [aos] add crawler

---
+++ Android/ExhibitionInfoLib/app/src/main/java/com/nezhitsya/exhibitioninfolib/StringExtension.kt
@@ -0,0 +1,16 @@
+package com.nezhitsya.exhibitioninfolib
+
+class StringExtension {
+
+    fun htmlEncodedString(s: String): String {
+        var yourNewString = s.toString().replace("\\n", "")
+        yourNewString = yourNewString.replace("\\t", "")
+        yourNewString = yourNewString.replace("&quot;", "")
+        yourNewString = yourNewString.replace("&amp;", "&")
+        yourNewString = yourNewString.replace("&lt;", "<")
+        yourNewString = yourNewString.replace("&gt;", ">")
+        yourNewString = yourNewString.replace("\\", "")
+        return yourNewString
+    }
+
+}
